12dd93f3c2fc8100be9bf19f9d17f32b03e57720
Antonio Cláudio Holanda Borges Junior <claudiohbjunior@gmail.com>
2023-11-19 02:05:35 -0300

    1 commit

---
+++ menu.js
@@ -0,0 +1,170 @@
+$(document).ready(function () {
+    // Salvar a URL do vídeo quando o modal é aberto
+    $('#videoModal').on('show.bs.modal', function () {
+      var videoUrl = "https://www.youtube.com/embed/x6TxAwNv-mY?si=zQBnLAHpzyUlNb4h"; // Substitua pela sua URL do vídeo
+      $('.youtube-video').attr('src', videoUrl);
+    });
+
+    // Pausar o vídeo quando o modal é fechado
+    $('#videoModal').on('hidden.bs.modal', function () {
+      $('.youtube-video').attr('src', '');
+    });
+  });
+
+
+
+
+  $(document).ready(function () {
+    // Salvar a URL do vídeo quando o modal é aberto
+    $('#videoModal2').on('show.bs.modal', function () {
+      var videoUrl ="https://www.youtube.com/embed/ijbSrrHi8Ao?si=iOlGSjac1trcp_So"; // Substitua pela sua URL do vídeo
+      $('.youtube-video2').attr('src', videoUrl);
+    });
+
+    // Pausar o vídeo quando o modal é fechado
+    $('#videoModal2').on('hidden.bs.modal', function () {
+      $('.youtube-video2').attr('src', '');
+    });
+  });
+
+
+
+
+  $(document).ready(function () {
+    // Salvar a URL do vídeo quando o modal é aberto
+    $('#videoModal3').on('show.bs.modal', function () {
+      var videoUrl = "https://www.youtube.com/embed/dhi2fSg5Bks?si=G_JaV2bNAzfBDkF0"; // Substitua pela sua URL do vídeo
+      $('.youtube-video3').attr('src', videoUrl);
+    });
+
+    // Pausar o vídeo quando o modal é fechado
+    $('#videoModal3').on('hidden.bs.modal', function () {
+      $('.youtube-video3').attr('src', '');
+    });
+  });
+
+
+
+  $(document).ready(function () {
+    // Salvar a URL do vídeo quando o modal é aberto
+    $('#videoModal4').on('show.bs.modal', function () {
+      var videoUrl = "https://www.youtube.com/embed/LfXFrdG8-eI?si=Xa_3E-Jmg75XjLls" ; // Substitua pela sua URL do vídeo
+      $('.youtube-video4').attr('src', videoUrl);
+    });
+
+    // Pausar o vídeo quando o modal é fechado
+    $('#videoModal4').on('hidden.bs.modal', function () {
+      $('.youtube-video4').attr('src', '');
+    });
+  });
+
+
+  $(document).ready(function () {
+    // Salvar a URL do vídeo quando o modal é aberto
+    $('#videoModal5').on('show.bs.modal', function () {
+      var videoUrl ="https://www.youtube.com/embed/VYcIqrWfQ6Y?si=4yTtZw_wY4ZooedP"; // Substitua pela sua URL do vídeo
+      $('.youtube-video5').attr('src', videoUrl);
+    });
+
+    // Pausar o vídeo quando o modal é fechado
+    $('#videoModal5').on('hidden.bs.modal', function () {
+      $('.youtube-video5').attr('src', '');
+    });
+  });
+
+
+  $(document).ready(function () {
+    // Salvar a URL do vídeo quando o modal é aberto
+    $('#videoModal6').on('show.bs.modal', function () {
+      var videoUrl = "https://www.youtube.com/embed/kduto8g0X5k?si=9PKlqoiHYIIFh_GS" ; // Substitua pela sua URL do vídeo
+      $('.youtube-video6').attr('src', videoUrl);
+    });
+
+    // Pausar o vídeo quando o modal é fechado
+    $('#videoModal6').on('hidden.bs.modal', function () {
+      $('.youtube-video6').attr('src', '');
+    });
+  });
+
+
+  $(document).ready(function () {
+    // Salvar a URL do vídeo quando o modal é aberto
+    $('#videoModal7').on('show.bs.modal', function () {
+      var videoUrl = "https://www.youtube.com/embed/yXEJADDf9Lg?si=9qBiNW5URE9DipBC"; // Substitua pela sua URL do vídeo
+      $('.youtube-video7').attr('src', videoUrl);
+    });
+
+    // Pausar o vídeo quando o modal é fechado
+    $('#videoModal7').on('hidden.bs.modal', function () {
+      $('.youtube-video7').attr('src', '');
+    });
+  });
+
+
+  $(document).ready(function () {
+    // Salvar a URL do vídeo quando o modal é aberto
+    $('#videoModal8').on('show.bs.modal', function () {
+      var videoUrl = "https://www.youtube.com/embed/0C3MnVIoFGM?si=L0jgZEA3VI9bPWEm"; // Substitua pela sua URL do vídeo
+      $('.youtube-video8').attr('src', videoUrl);
+    });
+
+    // Pausar o vídeo quando o modal é fechado
+    $('#videoModal8').on('hidden.bs.modal', function () {
+      $('.youtube-video8').attr('src', '');
+    });
+  });
+
+
+  $(document).ready(function () {
+    // Salvar a URL do vídeo quando o modal é aberto
+    $('#videoModal9').on('show.bs.modal', function () {
+      var videoUrl = "https://www.youtube.com/embed/kgU9DlfNPC0?si=IWjLOvSQRQwmdkeB" ; // Substitua pela sua URL do vídeo
+      $('.youtube-video9').attr('src', videoUrl);
+    });
+
+    // Pausar o vídeo quando o modal é fechado
+    $('#videoModal9').on('hidden.bs.modal', function () {
+      $('.youtube-video9').attr('src', '');
+    });
+  });
+
+
+
+  $(document).ready(function () {
+    // Salvar a URL do vídeo quando o modal é aberto
+    $('#videoModal').on('show.bs.modal', function () {
+      var videoUrl = "https://www.youtube.com/embed/x6TxAwNv-mY?si=zQBnLAHpzyUlNb4h"; // Substitua pela sua URL do vídeo
+      $('.youtube-video').attr('src', videoUrl);
+    });
+
+    // Pausar o vídeo quando o modal é fechado
+    $('#videoModal').on('hidden.bs.modal', function () {
+      $('.youtube-video').attr('src', '');
+    });
+  });
+
+
+  $(document).ready(function () {
+    // Salvar a URL do vídeo quando o modal é aberto
+    $('#videoModal10').on('show.bs.modal', function () {
+        var videoUrl = "https://www.youtube.com/embed/9rpRlZfJHQ8?si=BDOzsyWoYNf1Hn2_" ; // Substitua pela sua URL do vídeo
+        $('.youtube-video10').attr('src', videoUrl);
+    });
+
+    // Pausar o vídeo quando o modal é fechado
+    $('#videoModal10').on('hidden.bs.modal', function () {
+        $('.youtube-video10').attr('src', '');
+    });
+});
+
+
+
+function showSubMenu() {
+  var submenu = document.getElementById('contatoSubMenu');
+  submenu.style.display = 'block';
+}
+
+function hideSubMenu() {
+  var submenu = document.getElementById('contatoSubMenu');
+  submenu.style.display = 'none';
+}
